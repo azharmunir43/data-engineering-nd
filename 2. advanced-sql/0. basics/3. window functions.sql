2a930647d03d6e44ce8e6d38f2849cd0d80992c2
Azhar Munir <azharmunir123@gmail.com>
2019-12-29 15:31:20 +0500

    Oracle Example of Window Functions

---
+++ 2. advanced-sql/0. basics/3. window functions.sql
@@ -0,0 +1,35 @@
+--**----------------------------------------------------**--
+-- Window Functions
+
+--   A GROUP BY combined with aggregation compacts result in
+--   one row. However, window function can be used to produce
+--   result for each row.
+--**----------------------------------------------------**--
+SELECT
+    employee_id,
+    first_name,
+    salary,
+    department_id,
+    AVG(salary) OVER() ovrall_avg_sal,
+    AVG(salary) OVER(
+        PARTITION BY department_id
+    ) dpartment_avg_sal
+FROM
+    employees;
+
+--**----------------------------------------------------**--
+-- NOTES
+--**----------------------------------------------------**--
+
+--* Each window operation in the query is signified by inclusion of an OVER clause that specifies how to 
+--  partition query rows into groups for processing by the window function:
+--*    * The first OVER clause is empty, which treats the entire set of query rows as a single partition. 
+--       The window function thus produces a global sum, but does so for each row.
+--*    * The second OVER clause partitions rows by country, producing a sum per partition (per country). 
+--       The function produces this sum for each partition row.
+
+
+--** OTHER AGG FUNCTIONS include MIN, MAX, SUM etc. However SQL Server and MySQL also support some interesting functions like:
+--              * RANK()
+--              * DENSE_RANK()
+--              * ROW_NUMBER()
