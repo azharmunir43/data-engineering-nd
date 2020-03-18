de1010049ec5f888426a064c076395b50ed09611
Muhammad Azhar Munir <azharmunir123@gmail.com>
2020-03-18 09:01:53 +0500

    Completed subqueries

--- 2. advanced-sql/5. advanced subqueries/0.advanced subqueries.sql
+++ 2. advanced-sql/5. advanced subqueries/0.advanced subqueries.sql
@@ -70,8 +70,79 @@ FROM
     employees     e
     NATURAL JOIN departments   d;
     
-    
-    
 --**----------------------------------------------------**--
--- 
+-- Multiple Column Subquery & pairwise Comparison
 --**----------------------------------------------------**--
+SELECT first_name
+	,last_name
+	,manager_id
+	,department_id
+FROM employees
+WHERE (department_id, manager_id) -- pairwise comparison
+	IN (
+		SELECT department_id
+			,manager_id
+		FROM employees
+		WHERE UPPER(first_name) = 'AZHAR'
+		);
+
+--**----------------------------------------------------**--
+-- Correlated Subqueries
+--**----------------------------------------------------**--
+SELECT employee_id
+	,first_name
+	,last_name
+	,department_id
+	,salary
+FROM employees a
+WHERE salary = (
+		SELECT MAX(sa1ary)
+		FROM employees b
+		WHERE b.department_id = a.department_id
+		);
+
+--**----------------------------------------------------**--
+-- EXISTS Operator
+--**----------------------------------------------------**--
+SELECT employee_id
+	,department_id
+WHERE EXISTS (
+		SELECT 1
+			,employee_id
+		FROM EMPLOYEES
+		WHERE MANAGER_ID = A.EMPLOYEE_ID
+		);
+
+--**----------------------------------------------------**--
+-- NOT EXISTS Operator
+--**----------------------------------------------------**--
+
+SELECT department_id, department_name
+FROM departments d 
+WHERE NOT EXISTS (SELECT department_id
+                 FROM employees
+                 WHERE department_id = d.department_id)
+
+--**----------------------------------------------------**--
+-- Correlated Delete
+--**----------------------------------------------------**--
+
+DELETE FROM employees
+WHERE department_id IN 
+	(SELECT department_id 
+     FROM departments d NATURAL JOIN locations 1 
+     WHERE country_id = 'UK' ) ;
+
+--**----------------------------------------------------**--
+-- Correlated Update
+--**----------------------------------------------------**--
+UPDATE employees e_main
+SET (e_main.salary, e_main.commission_pct) = (
+		SELECT AVG(sa1ary)
+			,AVG(e.commission_pct)
+		FROM employees e
+		JOIN departments d ON e.department_id = d.department_id
+		WHERE e_main.department_id = d.department_id
+		GROUP BY d.department_id
+		)
+
