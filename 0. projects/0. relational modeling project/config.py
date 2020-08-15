d29583b4ae25a76df47d247f2488006318395f3d
Azhar Munir <azharmunir123@gmail.com>
2020-08-15 06:28:38 +0500

    Initial commit for postgresql project

---
+++ 0. projects/0. relational modeling project/config.py
@@ -0,0 +1,8 @@
+
+TARGET_DB_CREDS = {'user':"postgres", 'password':"techx007", 'host':"localhost", 'port':"5432", 'database':"mydb"}
+
+SRC_FOLDER_SONGS = r'data\song_data'
+SRC_FOLDER_LOGS = r'data\log_data'
+
+TEMP_DIR = r'temp\\'
+
