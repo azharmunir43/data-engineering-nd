5a893613a16f427e88f39453082a6112bb74723c
Azhar Munir <azharmunir123@gmail.com>
2020-08-15 18:45:27 +0500

    Added test cases for udacity's exercise queries

--- 0. projects/1. non-relational modeling project/testing/udacity.py
+++ 0. projects/1. non-relational modeling project/testing/udacity.py
@@ -1,8 +1,52 @@
 import unittest
+from db.sparkifydb import SparkifyCassandraDB
+from config import CASSANDRA_NODES as nodes
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class UdacityQueries(unittest.TestCase):
+
+    def test_udacity_1st_query(self):
+        expected = 1
+
+        cmd = 'SELECT COUNT(*) as c FROM songplaysBySession WHERE session_id = 338 AND item_in_session = 4'
+
+        res = self.run_db_command(cmd)
+
+        actual = res.one().c
+        self.assertEqual(expected, actual)
+
+    def test_udacity_2nd_query(self):
+        expected = 4
+
+        cmd = 'SELECT COUNT(*) AS c FROM songplaysByUser WHERE user_id = 10 AND session_id = 182'
+        res = self.run_db_command(cmd)
+
+        actual = res.one().c
+        self.assertEqual(expected, actual)
+
+    def test_udacity_3rd_query(self):
+        expected = 3
+
+        cmd = "SELECT COUNT(*) as c FROM listenersBySong WHERE song_title = 'All Hands Against His Own'"
+
+        res = self.run_db_command(cmd)
+
+        actual = res.one().c
+        self.assertEqual(expected, actual)
+
+    def run_db_command(self, cmd):
+        keyspace = 'sparkify'
+        db = SparkifyCassandraDB(nodes, keyspace)
+        session = db.get_cluster_session()
+
+        res = db.execute_command(session, cmd, keyspace)
+
+        session.shutdown()
+        return res
+
+def main():
+    unittest.main()
+
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
